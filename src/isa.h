51cbb81190237b2a49868911e73c79aa318234f4
Gaurav Jain <43726919+gauravjain2@users.noreply.github.com>
2026-01-06 11:10:40 +0530

    updated isa.h

--- src/isa.h
+++ src/isa.h
@@ -1,25 +1,25 @@
 #ifndef ISA_H
 #define ISA_H
 
-// Data Movement [cite: 21]
+// Data Movement
 #define OP_PUSH 0x01
 #define OP_POP 0x02
 #define OP_DUP 0x03
 #define OP_HALT 0xFF
 
-// Arithmetic & Logic [cite: 24]
+// Arithmetic & Logic
 #define OP_ADD 0x10
 #define OP_SUB 0x11
 #define OP_MUL 0x12
 #define OP_DIV 0x13
 #define OP_CMP 0x14
 
-// Control Flow [cite: 28]
+// Control Flow
 #define OP_JMP 0x20
 #define OP_JZ 0x21
 #define OP_JNZ 0x22
 
-// Memory & Functions [cite: 31]
+// Memory & Functions
 #define OP_STORE 0x30
 #define OP_LOAD 0x31
 #define OP_CALL 0x40
